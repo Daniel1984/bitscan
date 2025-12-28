37e188e54a9f1eeda66f025c8630a00b9bc2e553
Daniel1984 <aciddaniel@gmail.com>
2025-12-28 23:44:03 +0100

    updating main with more env vars

--- src/main.zig
+++ src/main.zig
@@ -17,7 +17,11 @@ pub fn main() !void {
     const allocator = gpa.allocator();
 
     var env = Env.init(allocator);
-    const port: u16 = env.getInt(u16, "PORT", 5888);
+    const api_port: u16 = env.getInt(u16, "PORT", 5888);
+    const zmq_endpoint = env.getString("ZMQ_ENDPOINT", "tcp://127.0.0.1:28333");
+    defer allocator.free(zmq_endpoint);
+    const btc_rest_endpoint = env.getString("BTC_REST_ENDPOINT", "http://127.0.0.1:38332/rest");
+    defer allocator.free(btc_rest_endpoint);
 
     const dbpool = try Db.init(allocator);
     defer dbpool.deinit();
@@ -25,23 +29,23 @@ pub fn main() !void {
 
     var app = App.init(dbpool, allocator);
 
-    var server = try httpz.Server(*App).init(allocator, .{ .port = port, .address = "0.0.0.0" }, &app);
+    var server = try httpz.Server(*App).init(allocator, .{ .port = api_port, .address = "0.0.0.0" }, &app);
     defer server.deinit();
     defer server.stop();
 
     var router = try server.router(.{});
     router.get("/status", handlers.getStatus, .{});
 
-    var zmq_stream = try Stream.init(allocator, .{ .stream_url = "tcp://127.0.0.1:28333" });
+    var zmq_stream = try Stream.init(allocator, .{ .stream_url = zmq_endpoint });
     defer zmq_stream.deinit();
 
-    msg_processor = MsgProcessor.init(allocator, dbpool);
+    msg_processor = MsgProcessor.init(allocator, dbpool, btc_rest_endpoint);
     defer msg_processor.deinit();
 
     const stream_thread = try std.Thread.spawn(.{}, consumeStream, .{&zmq_stream});
     defer stream_thread.join();
 
-    std.log.info("server started at port: {d}", .{port});
+    std.log.info("server started at port: {d}", .{api_port});
     try server.listen();
 }
 
