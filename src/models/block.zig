9b8ece95aa7055ebe1c33c6b7ad528ee7e4b3f79
Daniel1984 <aciddaniel@gmail.com>
2025-12-28 23:29:13 +0100

    structs for mapping block data

---
+++ src/models/block.zig
@@ -0,0 +1,57 @@
+const std = @import("std");
+const json = std.json;
+
+pub const ScriptPubKey = struct {
+    @"asm": ?[]const u8 = null,
+    hex: ?[]const u8 = null,
+    address: ?[]const u8 = null,
+    type: ?[]const u8 = null,
+};
+
+pub const Output = struct {
+    value: f64,
+    n: u32,
+    scriptPubKey: ScriptPubKey,
+};
+
+pub const Input = struct {
+    // common fields
+    sequence: u64,
+    txinwitness: ?[]json.Value = null,
+
+    // fields for regular transactions
+    txid: ?[]const u8 = null,
+    vout: ?u32 = null,
+    scriptSig: ?json.Value = null,
+    prevout: ?Prevout = null,
+
+    // fields for regular transactions
+    coinbase: ?[]const u8 = null,
+};
+
+pub const Prevout = struct {
+    generated: bool,
+    height: u32,
+    value: f64,
+    scriptPubKey: ScriptPubKey,
+};
+
+pub const Transaction = struct {
+    txid: []const u8,
+    hash: []const u8,
+    version: i32,
+    size: u32,
+    vsize: u32,
+    weight: u32,
+    locktime: u64,
+    vin: []Input,
+    vout: []Output,
+};
+
+pub const BlockResponse = struct {
+    hash: []const u8,
+    confirmations: i32,
+    height: u32,
+    version: i32,
+    tx: []Transaction,
+};
