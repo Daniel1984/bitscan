e412ac549416a9b44909fe873017913536584cbf
Daniel1984 <aciddaniel@gmail.com>
2026-01-01 00:18:55 +0100

    cleaning up main.zig

--- src/main.zig
+++ src/main.zig
@@ -3,12 +3,11 @@ const httpz = @import("httpz");
 const Env = @import("./env.zig");
 const Db = @import("./db.zig").Db;
 const App = @import("./app.zig");
-const handlers = @import("./handlers/handlers.zig");
 const Stream = @import("./stream.zig");
 const StreamProcessor = @import("./streamprocessor.zig");
 const Backfill = @import("./backfill.zig");
-
-var msg_processor: StreamProcessor = undefined;
+const BlockProcessor = @import("./blockprocessor.zig").BlockProcessor;
+const Runtime = @import("./runtime.zig").Runtime;
 
 pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
@@ -28,39 +27,27 @@ pub fn main() !void {
     defer dbpool.deinit();
     try dbpool.ping();
 
-    var app = App.init(dbpool, allocator);
-
-    var server = try httpz.Server(*App).init(allocator, .{ .port = api_port, .address = "0.0.0.0" }, &app);
-    defer server.deinit();
-    defer server.stop();
-
-    var router = try server.router(.{});
-    router.get("/status", handlers.getStatus, .{});
-
     var zmq_stream = try Stream.init(allocator, .{ .stream_url = zmq_endpoint });
     defer zmq_stream.deinit();
 
-    msg_processor = StreamProcessor.init(allocator, dbpool, btc_rest_endpoint);
-    const stream_thread = try std.Thread.spawn(.{}, consumeStream, .{&zmq_stream});
-    defer stream_thread.join();
+    var backfill = Backfill.init(allocator, .{ .btc_rest_endpoint = btc_rest_endpoint });
+    var stream_processor = StreamProcessor.init(allocator);
 
-    const backfill = Backfill.init(allocator, .{
-        .btc_rest_endpoint = btc_rest_endpoint,
-        .from_block = 0,
-        .to_block = 10,
-    });
+    var block_processor = BlockProcessor.init(allocator, .{ .btc_rest_endpoint = btc_rest_endpoint, .db = dbpool });
+    defer block_processor.deinit();
 
-    const hash = try backfill.getBlockHash(0);
-    std.log.info("Block hash for height: {s}", .{hash});
+    var runtime = Runtime.init(
+        allocator,
+        &zmq_stream,
+        &stream_processor,
+        &backfill,
+        &block_processor,
+    );
+    defer runtime.deinit();
 
-    std.log.info("server started at port: {d}", .{api_port});
-    try server.listen();
-}
+    try runtime.start();
+    defer runtime.stop();
 
-fn handleMessageWrapper(topic: []const u8, msg: []const u8) void {
-    msg_processor.processMsg(topic, msg);
-}
-
-fn consumeStream(s: *Stream) void {
-    s.consume(handleMessageWrapper);
+    var app = App.init(allocator, dbpool);
+    try app.startApi(api_port);
 }
