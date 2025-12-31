a3917d3828c43af7409bb089835fb6ab53ddc736
Daniel1984 <aciddaniel@gmail.com>
2025-12-31 23:58:22 +0100

    updating stream processor

--- src/streamprocessor.zig
+++ src/streamprocessor.zig
@@ -11,9 +11,7 @@ pub fn init(a: std.mem.Allocator) StreamProcessor {
     };
 }
 
-pub fn processMsg(self: StreamProcessor, topic: []const u8, msg: []const u8) ![]u8 {
-    std.debug.print("[ZMQ] topic={s} size={}\n", .{ topic, msg.len });
-
+pub fn toHex(self: StreamProcessor, msg: []const u8) ![]u8 {
     var hex_string = try self.allocator.alloc(u8, msg.len * 2);
     defer self.allocator.free(hex_string);
 
@@ -21,6 +19,5 @@ pub fn processMsg(self: StreamProcessor, topic: []const u8, msg: []const u8) ![]
         _ = try std.fmt.bufPrint(hex_string[i * 2 .. i * 2 + 2], "{x:0>2}", .{byte});
     }
 
-    std.debug.print("[ZMQ] block hash hex: {s}\n", .{hex_string});
     return self.allocator.dupe(u8, hex_string);
 }
